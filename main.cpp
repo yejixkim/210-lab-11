b5974f1754fb87d74f043aa05cb9c40da7aa4a5a
Yeji Kim <yejkim.yk@gmail.com>
2026-09-19 22:23:55 -0700

    update 1

--- main.cpp
+++ main.cpp
@@ -5,6 +5,39 @@
 
 using namespace std;
 
+// this struct represents one lab worker
+// member variables will be employee ID, number of days worked, and number of samples processed
+struct LabWorker {
+    string name;
+    int employeeID;
+    int numberOfDays;
+    int *samplesProcessed;
+};
+
 int main () {
+    // ask how many workers we want to track
+    int numberOfWorkers;
+
+    cout << "How many lab workers would you like to enter? ";
+    cin >> numberOfWorkers;
+
+    // dynamically make an array of LabWorkers struct
+    LabWorker* workers = new LabWorker[numberOfWorkers];
+
+    // get info for each worker
+    for (int i = 0; i < numberOfWorkers; i++) {
+        cout << "\nWorker" << i + 1 << endl;
+
+        cout << "Enter employee ID: ";
+        cin >> workers[i].employeeID;
+
+        cout << "How many days worked? ";
+        cin >> workers[i].numberOfDays;
+
+        // create a dynamic array inside the struct
+        workers[i].samplesProcessed = new int[workers[i].numberOfDays];
+
+    }
+
     return 0;
 }
